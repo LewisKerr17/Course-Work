3c8238ec47b2e8763f5587f9e988c3c8b08daaf3
Lewis Kerr <lewis.kerr26@gmail.com>
2024-11-18 19:47:46 +0000

    numguess with CSV works fully

--- Python/numberGuess.py
+++ Python/numberGuess.py
@@ -4,13 +4,21 @@ import csv
 userScore = 0
 userName = input('Name: ')
 fieldNames = ['Name', 'Score']
-highScores = []
+highScoresWithName = []
+highScoresInt = []
 
 with open('Python/scores.csv', 'r') as csvfile:
     reader = csv.DictReader(csvfile, fieldnames=fieldNames)
+    next(reader)
     for row in reader:
-        highScores.append(row['Score'])
-        print(row['Name'], highScores)
+        storedScore = int(row['Score'])
+        storedName = row['Name']
+        highScoresWithName.append((storedName, storedScore))
+        highScoresInt.append(storedScore)
+
+highScoresWithName.sort(reverse=True)
+print('Top 5 scores')
+print(highScoresWithName[:5])
 
 print("I'm thinking of a number between 1 and 10, guess it")
 
@@ -22,7 +30,7 @@ while True:
     
     while playCount < 3:
         try:
-            userGuess = int(input(f"Guess #{playCount + 1}: "))
+            userGuess = int(input(f'Guess #{playCount + 1}: '))
             playCount += 1
 
             if (userGuess == numberToGuess):
@@ -51,12 +59,14 @@ while True:
             print("End of Game") 
             break 
 
+
     if (userGuess == numberToGuess):
         with open('Python/scores.csv', 'r') as csvfile:
             reader = csv.DictReader(csvfile, fieldnames=fieldNames)
-            if userScore >= highScores[:5]:
-                with open('Python/scores.csv', 'a') as csvfile:
+
+            if len(highScoresInt) < 5 or userScore > min(highScoresInt[:5]):
+                with open('Python/scores.csv', 'a', newline='') as csvfile:
                     writer = csv.DictWriter(csvfile, fieldnames=fieldNames)
-                    writer.writerow({userName, userScore})
+                    writer.writerow({'Name': userName, 'Score': userScore})
 
         break
